154ba6a5e441faa4b37f917588000b8689b9ba67
vxhien <vxhien96@gmail.com>
2019-11-05 19:50:27 +0700

    Doi ten title

--- routes/index.js
+++ routes/index.js
@@ -8,32 +8,32 @@ router.get('/', function (req, res, next) {
 
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'About us'});
+  res.render('about', { title: 'Barber', name: 'About us'});
 });
 
 /* GET service page. */
 router.get('/service', function (req, res, next) {
-  res.render('service', { title: 'Services'});
+  res.render('service', { title: 'Barber', name: 'Services'});
 });
 
 /* GET blog page. */
 router.get('/blog', function (req, res, next) {
-  res.render('blog', { title: 'Blog'});
+  res.render('blog', { title: 'Barber', name: 'Blog'});
 });
 
 /* GET single blog page. */
 router.get('/single-blog', function (req, res, next) {
-  res.render('single-blog', { title: 'Single Blog'});
+  res.render('single-blog', { title: 'Barber', name: 'Single Blog'});
 });
 
 /* GET element page. */
 router.get('/elements', function (req, res, next) {
-  res.render('elements', { title: 'Elements'});
+  res.render('elements', { title: 'Barber', name: 'Elements'});
 });
 
 /* GET service page. */
 router.get('/contact', function (req, res, next) {
-  res.render('contact', { title: 'Contact Us'});
+  res.render('contact', { title: 'Barber', name: 'Contact Us'});
 });
 
 module.exports = router;
